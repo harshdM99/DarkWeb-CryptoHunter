abc15f2d28f35d26589da50704628eb2b6780bfd
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2025-01-20 12:47:39 -0500

    add comments

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
+# Update based on your system 
 CRAWL_LIMIT = 1000
-TOR_PATH = "/opt/tor-browser_en-US/"
+TOR_PATH = "/opt/tor-browser_en-US/" # path to tor browser
 DB_NAME = "crawlerData1"
 URL = 'http://thehiddenwiki.org/'
