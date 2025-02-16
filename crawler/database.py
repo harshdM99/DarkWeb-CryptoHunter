8c9367d46a0e27dce3f5e6ac964d8ad109f19545
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2025-02-15 21:18:00 -0500

    update database config for docker

--- crawler/database.py
+++ crawler/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 class Database:
     def __init__(self, db_name):
-        self.client = MongoClient('localhost', 27017)
+        self.client = MongoClient('mongodb', 27017)
         self.db = self.client[db_name]
         self.table1 = self.db['websites']
         self.table2 = self.db['crawl_links']
